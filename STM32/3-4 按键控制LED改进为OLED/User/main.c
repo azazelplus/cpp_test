1cac406bd51dc585018ca81c63159eafda8de2f5
azazelplus <azazelplusplusplus@gmail.com>
2025-07-10 19:58:04 +0800

    autosave

---
+++ STM32/3-4 按键控制LED改进为OLED/User/main.c
@@ -0,0 +1,15 @@
+#include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+#include "OLED.h"
+
+int main(void)
+{
+
+	OLED_Init();
+	OLED_ShowChar(1,1,'A');
+	while (1)
+	{
+
+		
+	}
+}
