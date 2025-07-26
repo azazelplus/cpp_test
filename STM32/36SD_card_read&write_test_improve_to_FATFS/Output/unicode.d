0125aebba53e1b0064ea4933d59ff4ffbb339f50
azazelplus <azazelplusplusplus@gmail.com>
2025-07-26 16:59:43 +0800

    autosave

--- STM32/36SD_card_read&write_test_improve_to_FATFS/Output/unicode.d
+++
@@ -1,5 +0,0 @@
-..\..\output\unicode.o: ..\..\User\fafts\option\unicode.c
-..\..\output\unicode.o: ..\..\User\fafts\option\../ff.h
-..\..\output\unicode.o: ..\..\User\fafts\option\../integer.h
-..\..\output\unicode.o: ..\..\User\fafts\option\../ffconf.h
-..\..\output\unicode.o: ..\..\User\fafts\option\ccsbcs.c
