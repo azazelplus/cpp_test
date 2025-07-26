62f8109049c233b78c4ac2041f6db905acfcaffc
azazelplus <azazelplusplusplus@gmail.com>
2025-07-26 16:59:43 +0800

    autosave

--- STM32/36SD_card_read&write_test_improve_to_FATFS/Output/ff.d
+++
@@ -1,6 +0,0 @@
-..\..\output\ff.o: ..\..\User\fafts\ff.c
-..\..\output\ff.o: ..\..\User\fafts\ff.h
-..\..\output\ff.o: ..\..\User\fafts\integer.h
-..\..\output\ff.o: ..\..\User\fafts\ffconf.h
-..\..\output\ff.o: ..\..\User\fafts\diskio.h
-..\..\output\ff.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdarg.h
